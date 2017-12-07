54192d1fcd18b633da1ff782a32e25a0d4307327
Hannah Danziger <hdanziger8@gmail.com>
2017-12-07 17:38:08 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-students = Student.create([first_name: "Daenerys", last_name: "Targaryen"])
